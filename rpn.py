c0b869e208797e436a8ef0aea8346590357388ee
Antonio Thomas Atkinson <atatkins@umich.edu>
2017-10-27 12:43:45 -0400

    Refactored functions and imported operator

--- rpn.py
+++ rpn.py
@@ -1,22 +1,22 @@
-
 #!/usr/bin/env python3
+import operator
 
+ops = {
+	'+': operator.add,
+	'-': operator.sub
+}
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
-		if token == '+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(result)
-		else:
+		try:
 			stack.append(int(token))
-	print(stack)
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops[token]
+			result = function(arg1, arg2)
+			stack.append(result)
+	#print(stack)
 	return stack.pop()
 
 def main():
